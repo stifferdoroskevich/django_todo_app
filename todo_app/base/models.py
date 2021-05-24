1596dcff7e30a32cb8e018a71d22752f3b39b31e
Stiffer Doroskevich <stiffer17@gmail.com>
2021-05-24 19:41:28 -0400

    Added order to task list

--- todo_app/base/models.py
+++ todo_app/base/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import CASCADE
 
 
 class Task(models.Model):
@@ -12,3 +11,6 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['complete']
