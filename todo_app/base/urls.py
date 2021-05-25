f09f129d996b75578e2fb5023bd362574de5838f
Stiffer Doroskevich <stiffer17@gmail.com>
2021-05-24 22:21:01 -0400

    CRUD - Create operation

--- todo_app/base/urls.py
+++ todo_app/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskDetail, TaskList
+from .views import TaskCreate, TaskDetail, TaskList
 
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create_task/', TaskCreate.as_view(), name='create_task'),
 ]
