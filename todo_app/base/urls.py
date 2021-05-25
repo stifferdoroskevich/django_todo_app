2552b4f8691d708c8dc5ebc3f239f26636f9a6be
Stiffer Doroskevich <stiffer17@gmail.com>
2021-05-24 22:41:35 -0400

    CRUD - Update Operation

--- todo_app/base/urls.py
+++ todo_app/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskCreate, TaskDetail, TaskList
+from .views import TaskCreate, TaskDetail, TaskList, TaskUpdate
 
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('create_task/', TaskCreate.as_view(), name='create_task'),
+    path('update_task/<int:pk>/', TaskUpdate.as_view(), name='update_task'),
 ]
--- todo_app/base/views.py
+++ todo_app/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -22,3 +22,8 @@ class TaskCreate(CreateView):
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
