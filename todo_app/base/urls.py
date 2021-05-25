5345b744a0290e3e7ad25470b9da4fcd38fb335a
Stiffer Doroskevich <stiffer17@gmail.com>
2021-05-24 21:05:33 -0400

    Added the task/id method and url

--- todo_app/base/urls.py
+++ todo_app/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskDetail, TaskList
 
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='tasks')
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 ]
