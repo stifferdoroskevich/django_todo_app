7f212230fb0f49005c30aba27a40b31ccd225dd2
Stiffer Doroskevich <stiffer17@gmail.com>
2021-05-25 21:26:50 -0400

    Added Login/Logout

--- todo_app/base/urls.py
+++ todo_app/base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TaskCreate, TaskDetail, TaskList, TaskUpdate, TaskDelete
-
+from .views import TaskCreate, TaskDetail, TaskList, TaskUpdate, TaskDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('create_task/', TaskCreate.as_view(), name='create_task'),
